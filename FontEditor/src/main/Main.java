63cffd6c316842606f0536bf3ffe90fa8e1c4b27
unkarjedy <unkarjedy@mail.ru>
2015-02-27 20:15:33 +0300

    Added FontEditor project

---
+++ FontEditor/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by user on 27.02.2015.
+ */
+public class Main {
+
+    public static void main(String[] argv){
+        System.out.print("Hello World!");
+    }
+
+
+}

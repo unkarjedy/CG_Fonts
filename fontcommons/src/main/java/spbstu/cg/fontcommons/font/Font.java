3572ca5a34a310aefdd447f021633ebd41bd44a7
Egor Gorbunov <egor-mailbox@narod.ru>
2015-04-26 15:10:56 +0300

    struct. changes

---
+++ fontcommons/src/main/java/spbstu/cg/fontcommons/font/Font.java
@@ -0,0 +1,37 @@
+package spbstu.cg.fontcommons.font;
+
+import java.util.*;
+
+/**
+ * Created by user on 28.02.2015.
+ */
+/* TODO: implement class */
+public class Font {
+    private String name;
+
+    private Map<Character, Letter> letters;
+
+    Font(String fontName) {
+        letters = new TreeMap<>();
+        // TODO: implement
+    }
+
+    public void addLetter(Character character, Letter letter){
+        if (letter == null || character == null)
+            throw new NullPointerException();
+
+        letters.put(character, letter);
+    }
+
+    public Letter getLetter(Character character) {
+        return letters.get(character);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

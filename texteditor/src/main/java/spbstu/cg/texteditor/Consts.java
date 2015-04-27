86c8140871d04573522beb12852e30085c01a4cf
unkarjedy <unkarjedy@mail.ru>
2015-04-27 23:39:33 +0430

    Merged ALL. Start here.

---
+++ texteditor/src/main/java/spbstu/cg/texteditor/Consts.java
@@ -0,0 +1,9 @@
+package spbstu.cg.texteditor;
+
+/**
+ * Created by JDima on 27/04/15.
+ */
+public class Consts {
+    public static final String INCORRECT_FORMAT = "Selected file has incorrect format!";
+    public static final String INCORRECT_FONT = "Text editor does not contain selected font!";
+}
